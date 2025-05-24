f1345286e16734fe8259a5e1bca135e4eae89a93
sᴠɴ <100134110+svnig7@users.noreply.github.com>
2025-05-24 15:05:49 +0530

    Update stream_routes.py

--- WOODStream/server/stream_routes.py
+++ WOODStream/server/stream_routes.py
@@ -137,5 +137,5 @@ async def media_streamer(request: web.Request, db_id: str):
 
 @routes.get("/file/{_id}")
 async def file_deeplink(request: web.Request):
-    _id = int(request.match_info["_id"])
+    file_id = request.match_info["file_id"]
     raise web.HTTPFound(f"https://t.me/{WOODStream.username}?start=file_{_id}")
